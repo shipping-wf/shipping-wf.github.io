a0c9ddcbdb4abd0d1e95649513f78db92c468cd5
Balaji Viswanathan <bviswana@in.ibm.com>
2020-09-06 23:38:01 +0530

    More fixes. PartyRef gets its own IRI

--- app/routes/terms.js
+++ app/routes/terms.js
@@ -4,6 +4,8 @@ var axios = require('axios');
 
 var router = express.Router();
 
+const LINK_HEADER_CONTEXT = 'http://www.w3.org/ns/json-ld#context';
+
 // grab the built-in Node.js doc loader
 const nodeDocumentLoader = jsonld.documentLoaders.node();
 
@@ -13,53 +15,75 @@ const customLoader = async (url, options) => {
 	if (false) {
 		return {
 			contextUrl: null, // this is for a context via a link header
-			document: request.get("https://shipping-wf.github.io/BillOfLading.jsonld"), // this is the actual document that was loaded
+			document: await axios.get("https://shipping-wf.github.io/BillOfLading.jsonld"), // this is the actual document that was loaded
 			documentUrl: "https://shipping-wf.github.io/BillOfLading.jsonld" // this is the actual context URL after redirects
 		};
 	}
 	return nodeDocumentLoader(url);
 };
 
+var getLinkHeader = function(req) {
+  var contextUrl = null;
+  if (req.headers['content-type'] !== 'application/ld+json' && req.headers.link != undefined ) {
+      const linkHeaders = jsonld.parseLinkHeader(req.headers.link);
+      const linkedContext = linkHeaders[LINK_HEADER_CONTEXT];
+
+       if(linkedContext) {
+        contextUrl = linkedContext.target;
+      }
+  }
+
+  return contextUrl;
+}
+
 var expand = async function(req, res) { 
   // Your async task will execute with await
   let context = req.body["@context"];
   if (context == null) { 
-	context = "https://shipping-wf.github.io/BillOfLading.jsonld";
+	context = getLinkHeader(req);
   }
   const expanded = await jsonld.expand(req.body, {expandContext: context});
+  res.set('content-type','application/json+ld');
   res.send(expanded);
 }
 
 var flatten = async function(req, res) {
    const flattened = await jsonld.flatten(req.body);
+   res.set('content-type','application/json+ld');
    res.send(flattened);
 }
 
 var compact = async function(req, res) {
   let context = req.body["@context"];
   if (context == null) { 
-	var doc = await axios.get("https://shipping-wf.github.io/BillOfLading.jsonld")	
+	var contexturl = getLinkHeader(req);
+	var doc = await axios.get(contextUrl);	
 	context = doc.data;
   }
   console.log("Context is : " + JSON.stringify(context));
 
   const compacted = await jsonld.compact(req.body, context);
+  res.set('content-type','application/json+ld');
   res.send(compacted);
 }
 
 router.post('/expand', function(req, res, next) {
-	console.log("Serving POST");
+	console.log("Serving EXPAND: ");
 	expand(req, res)
 });
 
 router.post('/flatten', function(req, res, next) {
-	console.log("Serving POST");
+	console.log("Serving FLATTEN");
 	flatten(req, res)
 });
 
 router.post('/compact', function(req, res, next) {
-	console.log("Serving POST");
+	console.log("Serving COMPACT");
 	compact(req, res)
+});
+
+router.post('/frame', function(req, res, next) {
+	console.log("Serving FRAME")
 });
 
 module.exports = router;
